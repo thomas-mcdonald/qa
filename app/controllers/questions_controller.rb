32187a571fb27b003c837fa581685248ae099b57
Thomas McDonald <tom@conceptcoding.co.uk>
2011-09-01 12:27:54 +0100

    First pass at recent tags

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,8 @@ class QuestionsController < ApplicationController
   
   def index
     @questions = Question.order('updated_at DESC').includes(:tags, :user, :votes).page(params[:page])
+
+    @recent_tags = Tag.select('tags.*, count(taggings.tag_id) as count').joins(:taggings).where('taggings.created_at > ?', 7.days.ago).group('taggings.tag_id').order('count(taggings.tag_id) DESC').limit(10).all
   end
 
   def show
