9630dfa2da49daadeb69dabca8629506eac1ba77
Thomas McDonald <tom@conceptcoding.co.uk>
2011-09-03 21:30:13 +0100

    Added accessors for badge names & descriptions

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -6,4 +6,18 @@ class Badge < ActiveRecord::Base
 
   belongs_to :source, :polymorphic => true
   belongs_to :user
+
+  default_scope order('created_at DESC')
+
+  def self.recent
+    limit(10)
+  end
+
+  def name
+    I18n.t("badges.#{token}.name")
+  end
+
+  def description
+    I18n.t("badges.#{token}.description")
+  end
 end
