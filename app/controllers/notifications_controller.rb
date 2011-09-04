c37e16cdc2cc3657203758da05c88c1bffc74b29
Thomas McDonald <tom@conceptcoding.co.uk>
2011-09-04 14:54:31 +0100

    Refactored dismiss into code on notification Model

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,6 @@
 class NotificationsController < ApplicationController
   def dismiss
-    n = Notification.find(params[:id])
-    n.dismissed = true
-    n.save
+    Notification.dismiss!(params[:id])
     respond_to do |format|
       format.html { redirect_to "/" }
       format.json { render :json => { :dismiss => n.id }.to_json }
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,10 @@ class Notification < ActiveRecord::Base
   belongs_to :user
   serialize :parameters
   scope :active, where('dismissed = ?', false)
+
+  def self.dismiss!(id)
+    n = Notification.find(id)
+    n.dismiss = true
+    n.save
+  end
 end
