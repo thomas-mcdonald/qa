6593b912166f500c1d44e742cdb6c991184f79d7
Thomas McDonald <tom@conceptcoding.co.uk>
2011-09-04 14:48:36 +0100

    Added notification for badges that have a question as their source

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -10,4 +10,22 @@ module NotificationHelper
       I18n.t("notifications.#{notification.token}", notification.parameters).html_safe
     end
   end
+
+  def new_badge_with_question_as_source(n)
+    badge = Badge.new_from_token(n.parameters[:badge_token])
+    notibox n do
+      n.parameters[:badge_link] = link_to badge.name, badge_url(badge)
+      n.parameters[:source_link] = link_to n.parameters[:title], question_url(n.parameters[:id])
+      I18n.t("notifications.new_badge_with_question_as_source", n.parameters).html_safe
+    end
+  end
+
+  def new_badge_without_source(n)
+    badge = Badge.new_from_token(n.parameters[:badge_token])
+    notibox n do
+      n.parameters[:badge_link] = link_to badge.name, badge_url(badge)
+      n.parameters[:link] = link_to "your profile", user_url(n.user)
+      I18n.t("notifications.new_badge_without_source", n.parameters).html_safe
+    end
+  end
 end
