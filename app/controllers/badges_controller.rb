dc723f9b7e5829a8e567da65ee63477379e4e984
Thomas McDonald <tom@conceptcoding.co.uk>
2011-09-04 15:05:18 +0100

    Badges#show should redirect to the appropriate badge when an ID is passed as :id

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -5,8 +5,12 @@ class BadgesController < ApplicationController
   end
   
   def show
-    @badge = Badge.new_from_param(params[:id])
-    @count = Badge.param_token(params[:id]).count
-    @badges = Badge.param_token(params[:id]).page(params[:page])
+    if params[:id].to_i == 0
+      @badge = Badge.new_from_param(params[:id])
+      @count = Badge.param_token(params[:id]).count
+      @badges = Badge.param_token(params[:id]).page(params[:page])
+    else
+      redirect_to Badge.find(params[:id])
+    end
   end
 end
