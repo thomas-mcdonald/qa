47d98ca1d5cb65fbc6e68a13d5fce77ab9d9175f
Thomas McDonald <tom@conceptcoding.co.uk>
2011-09-05 11:18:06 +0100

    Not really happy with this as it currently is, but added notifications for badges

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -20,6 +20,15 @@ module NotificationHelper
     end
   end
 
+  def new_badge_with_answer_as_source(n)
+    badge = Badge.new_from_token(n.parameters[:badge_token])
+    notibox n do
+      n.parameters[:badge_link] = link_to badge.name, notification_url(n)
+      n.parameters[:source_link] = link_to n.parameters[:question_title], question_url(n.parameters[:question_id])
+      I18n.t("notifications.new_badge_with_answer_as_source", n.parameters).html_safe
+    end
+  end
+
   def new_badge_without_source(n)
     badge = Badge.new_from_token(n.parameters[:badge_token])
     notibox n do
--- lib/async/badges/base.rb
+++ lib/async/badges/base.rb
@@ -2,11 +2,40 @@ module Async
   module Badges
     class Base
       def self.create_badge(token, user, source)
-        badge = Badge.new
-        badge.token = token
-        badge.user = user
-        badge.source = source
-        badge.save
+        badge = Badge.create(
+          :source => source,
+          :token => token,
+          :user => user
+        )
+        if badge.source == nil
+          Notification.create(
+            :token => 'new_badge_without_source',
+            :parameters => {
+              :badge_token => badge.token
+            },
+            :redirect => badge,
+            :user => badge.user
+          )
+        else
+          n = Notification.new(
+            :parameters => {
+              :badge_token => badge.token
+            },
+            :redirect => badge,
+            :token => "new_badge_with_#{badge.source_type.downcase}_as_source",
+            :user => badge.user
+          )
+          case badge.source_type
+          when "Question"
+            n.parameters[:title] = badge.source.title
+            n.parameters[:id] = badge.source.id
+          when "Answer"
+            n.parameters[:question_id] = badge.source.question.id
+            n.parameters[:question_title] = badge.source.question.title
+            n.parameters[:id] = badge.source.id
+          end
+          n.save
+        end
         badge
       end
     end
