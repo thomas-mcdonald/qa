2d7c10ae8714b04c22e47f90d91cd23072b36073
Thomas McDonald <tom@conceptcoding.co.uk>
2011-10-03 23:38:53 +0100

    Added tag extraction to text parser

--- lib/text_parser.rb
+++ lib/text_parser.rb
@@ -1,15 +1,26 @@
 module QA
   class TextParser
+    include Rails.application.routes.url_helpers
+
     def initialize(text)
       @text = text
     end
 
     def format
-      self.markdown.html_safe
+      markdown
+      extract_tags
+      @text.html_safe
     end
 
     def markdown
-      Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(@text)
+      @text = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(@text)
+    end
+
+    def extract_tags
+      matches = @text.scan /\[tag:(\S+)\]/
+      matches.each do |m|
+        @text.gsub! "[tag:#{m[0]}]", %(<a href="#{tagged_questions_path(m[0])}" class="btn tag">#{m[0]}</a>)
+      end
     end
   end
 end
