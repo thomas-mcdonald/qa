e8d31ba325dca22a31e2eb48da2853a8b59e05ff
Thomas McDonald <tom@conceptcoding.co.uk>
2011-10-04 08:41:11 +0100

    Add logged_in? helper for abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,27 +2,32 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
+    @user ||= User.new
     #
     # Posts
     #
     can :read, Question do |q|
-      false
-      true unless q.deleted? && user.reputation < 1000
+      true unless q.deleted? && logged_in? && @user.reputation < 1000
     end
     can :create, Question
     can :update, Question do |q|
       false
-      true if q.user_id == user.id
-      true if user.reputation > 500
+      next unless logged_in?
+      true if q.user_id == @user.id
+      true if @user.reputation > 500
     end
     #
     # Votes
     #
     can :create, Vote do |vote|
       false
-      true if vote.value == 1 && user.can_upvote?
-      true if vote.value == -1 && user.can_downvote?
+      next unless logged_in?
+      true if vote.value == 1 && @user.can_upvote?
+      true if vote.value == -1 && @user.can_downvote?
     end
+  end
+
+  def logged_in?
+    !!@user.id
   end
 end
