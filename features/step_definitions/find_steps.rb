a16ae906f9efcae104ce0d8ca333f3ecb8c64af2
Thomas McDonald <tom@conceptcoding.co.uk>
2012-03-24 15:25:18 +0000

    stop arguments destroying textmate highlighting

--- features/step_definitions/find_steps.rb
+++ features/step_definitions/find_steps.rb
@@ -1,13 +1,13 @@
-When /^I click on the Google provider$/ do
+When(/^I click on the Google provider$/) do
   click_link_or_button("google-login")
 end
 
-Then /^I should see a series of links to login providers$/ do
-  page.should have_link("google-login", href: "/auth/google")
-  page.should have_link("twitter-login", href: "/auth/twitter")
+Then(/^I should see a series of links to login providers$/) do
+  should have_link("google-login", href: "/auth/google")
+  should have_link("twitter-login", href: "/auth/twitter")
 end
 
-Then /^I should see a title of "([^"]*)"$/ do |title|
+Then(/^I should see a title of "([^"]*)"$/) do |title|
   within(".page-header h1") do
     should have_content(title)
   end
--- features/step_definitions/path_steps.rb
+++ features/step_definitions/path_steps.rb
@@ -1,11 +1,11 @@
-Given /^I am on the homepage$/ do
+Given(/^I am on the homepage$/) do
   visit("/")
 end
 
-Given /^I am on the signup page$/ do
+Given(/^I am on the signup page$/) do
   visit("/signup")
 end
 
-Then /^I should be returned to the confirmation page$/ do
+Then(/^I should be returned to the confirmation page$/) do
   current_path.should == "/auth/google/callback"
 end
