f185bcab21b448115d0886182efd7e64fd73b0fa
Thomas McDonald <tom@conceptcoding.co.uk>
2012-03-24 14:08:48 +0000

    add callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Qa::Application.routes.draw do
   get '/signup', to: 'users#new'
+  get '/auth/:provider/callback', to: 'users#confirm'
 end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -4,4 +4,8 @@ describe "routing to users" do
   it "routes /signup to users#new" do
     { get: "/signup" }.should route_to(controller: "users", action: "new")
   end
+
+  it "routes authorisation callbacks to users#confirm" do
+    { get: '/auth/google/callback' }.should route_to(controller: 'users', action: 'confirm', provider: 'google')
+  end
 end
