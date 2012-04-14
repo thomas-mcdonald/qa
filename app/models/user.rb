c1b6a5e90a14db8c01651b75a4c2ac0c81506935
Thomas McDonald <tom@conceptcoding.co.uk>
2012-04-14 12:32:35 +0100

    use hash accessors for creating from omniauth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ActiveRecord::Base
 
   def self.new_from_hash(auth_hash)
     user = User.new(
-      email: auth_hash.info.email,
-      name: auth_hash.info.name
+      email: auth_hash[:info][:email],
+      name: auth_hash[:info][:name]
     )
     user.authorizations.new(
-      email: auth_hash.info.email,
-      provider: auth_hash.provider,
-      uid: auth_hash.uid
+      email: auth_hash[:info][:email],
+      provider: auth_hash[:provider],
+      uid: auth_hash[:uid]
     )
     user
   end
