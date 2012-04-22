f74c4aa886a4ebf9d1a3d2d6f86f92818f4728e1
Thomas McDonald <tom@conceptcoding.co.uk>
2012-04-22 22:10:47 +0100

    add rudimentary require_login filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def require_login
+    return true if logged_in?
+    redirect_to("/login")
+  end
+
   def login(user)
     session[:user_id] = user.id
   end
