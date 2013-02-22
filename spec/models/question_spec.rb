0efe85dab4decc111f693aad84167a84d6986381
Thomas McDonald <tom@conceptcoding.co.uk>
2013-02-22 14:35:51 +0000

    Add basic validation of attributes

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should ensure_length_of(:title).is_at_least(10).is_at_most(150) }
 end
