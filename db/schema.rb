fb19668c6688e4db9b16602948e8cb410f2ae482
Thomas McDonald <tom@conceptcoding.co.uk>
2013-03-09 21:51:36 +0000

    i'm noob

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130309195746) do
+ActiveRecord::Schema.define(version: 20130309202731) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20130309195746) do
     t.integer  "vote_type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "post_type"
   end
 
 end
