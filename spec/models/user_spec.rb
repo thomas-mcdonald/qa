8f00cced2fca42335f1095a54e9f8cf08a81f573
Thomas McDonald <tom@conceptcoding.co.uk>
2013-03-09 19:41:20 +0000

    tests for User#email_hash

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,30 @@ describe User do
   context 'associations' do
     it { should have_many(:authorizations) }
   end
+
+  describe 'email_hash' do
+    before do
+      @user = FactoryGirl.build(:user)
+    end
+
+    it 'should have a sane email hash' do
+      @user.email_hash.should =~ /^[0-9a-f]{32}$/
+    end
+
+    it 'should use downcase email' do
+      @user.email = "example@example.com"
+      @user2 = FactoryGirl.build(:user)
+      @user2.email = "ExAmPlE@eXaMpLe.com"
+
+      @user.email_hash.should == @user2.email_hash
+    end
+
+    it 'should trim whitespace before hashing' do
+      @user.email = "example@example.com"
+      @user2 = FactoryGirl.build(:user)
+      @user2.email = " example@example.com "
+
+      @user.email_hash.should == @user2.email_hash
+    end
+  end
 end
