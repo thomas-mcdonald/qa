a31120f03780177a4aab8a7f6152d031fc82b304
Thomas McDonald <tom@conceptcoding.co.uk>
2013-03-11 16:01:08 +0000

    Add Voteable#has_vote_by_user
    This will easily allow us to check if a user has alredy voted on an item with
    a given vote_type

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -11,5 +11,9 @@ module QA
     def vote_count
       self.votes.select { |v| [1,2].include? v.vote_type_id }.inject(0) { |sum, v| v.vote_type_id == 1 ? sum + 1 : sum - 1 }
     end
+
+    def has_vote_by_user(user, vote_type_id)
+      !self.votes.where(user_id: user.id, vote_type_id: vote_type_id).empty?
+    end
   end
 end
--- spec/concerns/voteable_examples.rb
+++ spec/concerns/voteable_examples.rb
@@ -22,4 +22,17 @@ shared_examples_for 'voteable' do
       item.vote_count.should == 1
     end
   end
+
+  context 'has_vote_by_user' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    it 'does not have a vote by default' do
+      item.has_vote_by_user(user, 1).should == false
+    end
+
+    it 'has a vote if there has been one created' do
+      item.votes << FactoryGirl.build(:upvote, user: user)
+      item.has_vote_by_user(user, 1).should == true
+    end
+  end
 end
