6ed3f4626ecd2c424f54a2ccc05390232ce40256
Thomas McDonald <tom@conceptcoding.co.uk>
2013-03-11 20:21:10 +0000

    votes/destroy partial does not require vote_type_id as a local
    We actually just use the vote_type_id of the partial, so there is basically
    no point in passing it in as well

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -5,14 +5,14 @@ module VotesHelper
     downvote = find_vote(@user_votes, post, Vote::DOWNVOTE)
     if !upvote.empty?
       # current upvote
-      ret << render(partial: 'votes/destroy', locals: { vote: upvote.first, vote_type_id: 1 })
+      ret << render(partial: 'votes/destroy', locals: { vote: upvote.first })
     else
       # not yet upvoted
       ret << render(partial: 'votes/create', locals: { post: post, vote_type_id: 1 })
     end
     ret << %(<p class="vote-count">#{ post.vote_count }</p>)
     if !downvote.empty?
-      ret << render(partial: 'votes/destroy', locals: { vote: downvote.first, vote_type_id: 2 })
+      ret << render(partial: 'votes/destroy', locals: { vote: downvote.first })
     else
       ret << render(partial: 'votes/create', locals: { post: post, vote_type_id: 2 })
     end
