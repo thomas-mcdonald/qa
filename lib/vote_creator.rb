eaf3fb6411a39237bab26bd07c56c54f93f9235e
Thomas McDonald <tom@conceptcoding.co.uk>
2013-05-22 13:39:41 +0100

    Rework VoteCreator. #create now creates and returns the vote.
    Updated tests and controller to use this.

--- lib/vote_creator.rb
+++ lib/vote_creator.rb
@@ -1,13 +1,10 @@
 class VoteCreator
-  attr_reader :recalculate
-
   # Creates a vote
   # user - the user creating the vote
   # opts - hash of options
   #  post_id   - id of the post
   #  post_type - type of post
   #  type_id - type of vote
-  #  recalculate : Boolean - true - do we recalculate the users reputation?
   def initialize(user, opts = {})
     raise ArgumentError unless user
     [:post_id, :post_type, :vote_type_id].each { |k| raise ArgumentError, "#{k} is missing" unless opts[k] }
@@ -17,6 +14,17 @@ class VoteCreator
   end
 
   def create
-    @user.votes.new(@vote_params)
+    @vote = @user.votes.new(@vote_params)
+    self.save
+    @vote
+  end
+
+  def save
+    result = false
+    Vote.transaction do
+      result = @vote.save
+      @vote.post.update_vote_count!
+    end
+    result
   end
 end
