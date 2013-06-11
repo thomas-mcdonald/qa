0644f7bae0c85402bf0b4d85cbe7c0c2d4c8869d
Thomas McDonald <tom@conceptcoding.co.uk>
2013-06-11 23:26:02 +0100

    Fix VoteCreator specs

--- spec/concerns/vote_creator_spec.rb
+++ spec/concerns/vote_creator_spec.rb
@@ -10,13 +10,14 @@ describe VoteCreator do
   end
 
   it 'requires some other non-nil parameters' do
-    -> { VoteCreator.new(user, post_id: nil, post_type: Question, vote_type_id: 1) }.should raise_error(ArgumentError)
-    -> { VoteCreator.new(user, post_id: 1, post_type: Question, vote_type_id: nil) }.should raise_error(ArgumentError)
+    -> { VoteCreator.new(user, post_id: nil, post_type: 'Question', vote_type_id: 1) }.should raise_error(ArgumentError)
+    -> { VoteCreator.new(user, post_id: 1, post_type: 'Question', vote_type_id: nil) }.should raise_error(ArgumentError)
     -> { VoteCreator.new(user, post_id: 1, post_type: nil, vote_type_id: 1) }.should raise_error(ArgumentError)
   end
 
   it 'creates a standard vote' do
-    vote = VoteCreator.new(user, post_id: 1, post_type: Question, vote_type_id: 1)
+    question = FactoryGirl.create(:question)
+    vote = VoteCreator.new(user, post_id: question.id, post_type: 'Question', vote_type_id: 1)
     vote.create.should be_a(Vote)
   end
 end
