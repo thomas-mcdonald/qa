148119f0c348d5948d11079351bb42a455bc2a27
Thomas McDonald <tom@conceptcoding.co.uk>
2013-08-19 17:12:37 +0100

    Add invert to TimelineEvent

--- app/models/timeline_event.rb
+++ app/models/timeline_event.rb
@@ -1,8 +1,10 @@
 class TimelineEvent < ActiveRecord::Base
   belongs_to :post, polymorphic: true
   has_many :timeline_actors
+  has_many :users, through: :timeline_actors
 
   POST_CREATE = 1
+  INVERT = ['post_create'].unshift(nil).freeze
 
   def self.on_post_create(post, user)
     event = TimelineEvent.new(post: post)
