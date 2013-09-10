015e419d2cb03681357feb6d5d61876fb58df0ce
Thomas McDonald <tom@conceptcoding.co.uk>
2013-09-10 08:47:15 +0100

    Update stylesheets to new Bootstrap mixins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-dropdown
-//= require bootstrap-tooltip
-//= require bootstrap-popover
+//= require bootstrap/dropdown
+//= require bootstrap/tooltip
+//= require bootstrap/popover
 //= require_tree .
