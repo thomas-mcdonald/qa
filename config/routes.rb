4e44f7d3102b298a11ba75688482e672501d0e62
Thomas McDonald <tom@conceptcoding.co.uk>
2013-10-02 23:02:40 +0100

    Add some flash handling to exceptions

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Qa::Application.routes.draw do
 
   get '/users/:id/:slug', to: 'users#show'
   resources :users, only: [:create, :show]
-  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#new', as: 'login'
   post '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   # omniauth callbacks
