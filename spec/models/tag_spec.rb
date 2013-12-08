cb6cde291f9748836481a0dcfe7baea001227c29
Thomas McDonald <tom@conceptcoding.co.uk>
2013-12-08 12:54:48 +0000

    Add some tests for related_tags

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,14 +1,31 @@
 require 'spec_helper'
 
+def tag_finder(name)
+  Tag.where(name: name).first
+end
+
 describe Tag do
   it { should have_many(:taggings) }
   it { should have_many(:questions) }
 
-  context '#named' do
+  describe '.named' do
     let(:tag) { FactoryGirl.create(:tag) }
 
     it 'returns the tag named as parameter' do
       tag.should == Tag.named(tag.name)
+    end
+  end
+
+  describe '#related' do
+    it 'find related tags by checking questions with that tag' do
+      q1 = FactoryGirl.create(:question, tag_list: 'cat, join-tag')
+      q2 = FactoryGirl.create(:question, tag_list: 'dog, join-tag')
+      related = tag_finder('join-tag').related_tags
+      related.should include(tag_finder('dog'))
+      related.should include(tag_finder('cat'))
+      crelated = tag_finder('cat').related_tags
+      crelated.should include(tag_finder('join-tag'))
+      crelated.should_not include(tag_finder('dog'))
     end
   end
 end
