99aea239c18d1981c99b1ca23a908ee7d4f03cb0
Thomas McDonald <tom@conceptcoding.co.uk>
2013-12-26 23:59:19 +0000

    Load Redis configuration from config/redis.yml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,10 @@
-$redis = Redis.new
+config = YAML.load(ERB.new(File.new("#{Rails.root}/config/redis.yml").read).result)[Rails.env]
+
+$redis = Redis.new(
+  host: config[:host],
+  port: config[:port],
+  password: config[:password],
+  db: config[:db]
+)
+
 $view = Redis::Namespace.new(:view, redis: $redis)
