aa17a677a25b6ac82c4d74da76d0072db53400a5
Thomas McDonald <tom@conceptcoding.co.uk>
2013-12-27 00:17:11 +0000

    Add an answer display to user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @questions = @user.questions.limit(5)
+    @answers = @user.answers.limit(5)
   end
 
   def new
