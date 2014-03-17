8f0bd483cade36c8d16e253480dd53f8459fe442
Thomas McDonald <tom@conceptcoding.co.uk>
2014-03-17 12:03:48 +0000

    fix reputation job

--- lib/jobs/calculate_reputation.rb
+++ lib/jobs/calculate_reputation.rb
@@ -2,7 +2,7 @@ module Jobs
   class CalculateReputation
     include Sidekiq::Worker
 
-    def self.perform(user_id)
+    def perform(user_id)
       User.find(user_id).calculate_reputation!
     end
   end
