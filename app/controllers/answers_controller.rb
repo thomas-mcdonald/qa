96f23dd2c805857fe9b2c0427aca47458c9fd961
Thomas McDonald <tom@conceptcoding.co.uk>
2014-04-05 17:38:57 +0100

    fix test suite

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -23,7 +23,7 @@ class AnswersController < ApplicationController
     @answer = Answer.find(params[:id])
     authorize(@answer)
     @answer.update_attributes(answer_params)
-    @answer.edit_timeline_event!
+    @answer.edit_timeline_event!(current_user)
     redirect_to @answer.question
   end
 
