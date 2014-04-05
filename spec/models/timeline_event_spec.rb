1370abbe848fd8a8e27a466c4f06292d5dd1a8e4
Thomas McDonald <tom@conceptcoding.co.uk>
2014-04-05 11:03:20 +0100

    prefer describe blocks over context for method blocks

--- spec/models/timeline_event_spec.rb
+++ spec/models/timeline_event_spec.rb
@@ -4,15 +4,16 @@ describe TimelineEvent do
   let(:question) { FactoryGirl.create(:question) }
   let(:user) { FactoryGirl.create(:user) }
 
-  context 'associations' do
+  describe 'associations' do
     it { should belong_to(:post) }
   end
 
-  context '#on_post_create' do
+  describe '#on_post_create' do
     it 'creates models as appropriate' do
       event = TimelineEvent.on_post_create(question, user)
       event.timeline_actors.size.should == 1
       event.timeline_actors.first.user.should == user
+      event.action.should == 'post_create'
     end
   end
 
