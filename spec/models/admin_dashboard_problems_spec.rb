36908d04d7adec0bc761b3b00cb56acf6ea3356d
Thomas McDonald <tom@conceptcoding.co.uk>
2014-08-12 22:32:01 +0100

    fix build

--- spec/models/admin_dashboard_problems_spec.rb
+++ spec/models/admin_dashboard_problems_spec.rb
@@ -7,12 +7,12 @@ describe AdminDashboardProblems do
     it 'detects if sidekiq workers are not running' do
       workers = Sidekiq::Workers.any_instance
       workers.expects(:size).returns(0)
-      expect(subject.problems).to have(1).item
+      expect(subject.problems.size).to be(1)
     end
 
     it 'does not insert if sidekiq is running' do
       Sidekiq::Workers.any_instance.stubs(:size).returns(5)
-      expect(subject.problems).to have(0).items
+      expect(subject.problems.size).to be(0)
     end
   end
 end
