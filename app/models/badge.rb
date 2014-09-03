288d3e53dae38d55fa17c1c531291252446ad7c0
Thomas McDonald <tom@conceptcoding.co.uk>
2014-09-03 22:45:05 +0100

    sketch of badge layout on homepage

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,12 @@
 class Badge < ActiveRecord::Base
   belongs_to :subject, polymorphic: true
   belongs_to :user
+
+  def badge_definition
+    QA::BadgeManager[self.name]
+  end
+
+  def name
+    self[:name].to_sym
+  end
 end
