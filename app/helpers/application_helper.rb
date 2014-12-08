07b0823264653584eea2f88a09a322ab385b2dbf
Thomas McDonald <tom@conceptcoding.co.uk>
2014-12-08 21:14:35 +0000

    add tabs to new user pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_tab(opts, &block)
+    if params.merge(opts) == params
+      %(<li class="active">#{capture(&block)}</li>).html_safe
+    else
+      %(<li>#{capture(&block)}</li>).html_safe
+    end
+  end
 end
