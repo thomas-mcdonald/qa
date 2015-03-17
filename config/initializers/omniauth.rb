c71321d75e0e692658682b165304a2790e1991f2
Thomas McDonald <tom@conceptcoding.co.uk>
2015-03-17 21:07:16 +0000

    add support for logging in with Twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,8 +8,14 @@ END
   abort
 end
 
+# TODO: check all used providers are enabled
+# TODO: check enabled providers have the correct environment variables set
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   if SiteSettings.authentication.google
     provider :google_oauth2, ENV['GOOGLE_CLIENT_KEY'], ENV['GOOGLE_CLIENT_SECRET'], name: 'google'
+  end
+  if SiteSettings.authentication.twitter
+    provider :twitter, ENV['TWITTER_CLIENT_KEY'], ENV['TWITTER_CLIENT_SECRET']
   end
 end
