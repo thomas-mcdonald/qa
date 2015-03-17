eb6acd116f6aff1b0be449ba34b804267ab0587e
Thomas McDonald <tom@conceptcoding.co.uk>
2015-03-17 16:59:31 +0000

    add support for enabling/disabling authentication providers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,15 @@
+# fatal error if no providers enabled
+if !SiteSettings['authentication'].any? { |k,v| v }
+  puts <<END
+    You must enable at least one authentication provider in site_settings.yml.
+
+END
+
+  abort
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_KEY'], ENV['GOOGLE_CLIENT_SECRET'], name: 'google'
+  if SiteSettings.authentication.google
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_KEY'], ENV['GOOGLE_CLIENT_SECRET'], name: 'google'
+  end
 end
