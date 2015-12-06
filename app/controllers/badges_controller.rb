77603c3d3953ed698267c0bf7049462d75a08f8c
Thomas McDonald <tom@conceptcoding.co.uk>
2015-12-06 12:37:58 +0000

    make badge list 3 columns

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -2,7 +2,7 @@ class BadgesController < ApplicationController
   def show
     @badge = QA::BadgeManager[param_to_sym]
     @badge_count = Badge.for(@badge.name).count
-    @recent_awards = Badge.for(@badge.name).joins(:user).limit(50)
+    @recent_awards = Badge.for(@badge.name).joins(:user).limit(60)
     render_404 if @badge.nil?
   end
 
