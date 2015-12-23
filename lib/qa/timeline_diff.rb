e80b48a3e728f8b7aab7b7f805605dc990288179
Thomas McDonald <tom@conceptcoding.co.uk>
2015-12-23 22:50:50 +0000

    add standard userbox rendering, add revision numbers

--- lib/qa/timeline_diff.rb
+++ lib/qa/timeline_diff.rb
@@ -2,20 +2,24 @@ module QA
   # TimelineDiff is to assist with creating the list of TimelineEvent objects.
   # Only some TimelineEvent objects have associated PostHistory objects, and these must be zipped together so they have access to previous one for diffing.
   class TimelineDiff
-    attr_reader :event, :previous
+    attr_reader :event, :previous, :revision
     delegate :action, :created_at, :post, :post_history, :users, to: :event
 
     # TODO: when we start tracking events with no post diff this will need updating
     def self.generate(timeline_events)
       previous = timeline_events.to_a.dup.unshift(nil)
+      revision = 1
       timeline_events.zip(previous).map do |tuple|
         # tuple is (current_event, previous_event)
-        TimelineDiff.new(tuple.first, tuple.second)
+        diff = TimelineDiff.new(revision, tuple.first, tuple.second)
+        revision += 1
+        diff
       end
     end
 
-    def initialize(event, previous)
+    def initialize(revision, event, previous)
       Differ.format = QA::Diff::HTML
+      @revision = revision
       @event = event
       @previous = previous
     end
