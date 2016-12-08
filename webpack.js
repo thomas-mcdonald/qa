2cc07c60350e5eacdb30798fed30c801350df31b
Thomas McDonald <tom@conceptcoding.co.uk>
2016-12-08 00:38:22 +0000

    hook up webpack to Rails

--- webpack.js
+++ webpack.js
@@ -1,7 +1,14 @@
 module.exports = {
   entry: './ui/App.js',
   output: {
-    path: __dirname + "/app",
+    path: __dirname + "/app/assets/javascripts",
     filename: "build.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
   }
 }
