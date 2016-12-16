8d497d77fd7c10643c974ed2e400535a6b38a7cc
Thomas McDonald <thomas-mcdonald@users.noreply.github.com>
2016-12-16 14:52:16 +0000

    add header and tabs to new_index

--- ui/questions/index/Container.js
+++ ui/questions/index/Container.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { Tab, Tabs, TabList, TabPanel } from '@blueprintjs/core'
 
 class Container extends Component {
   render() {
     return (
       <div id="page-wrap">
         <div id="body">
-          <div className="tabbed-header">
-            <h1>Recent Questions</h1>
-          </div>
+          <h1>Recent Questions</h1>
+          <Tabs>
+            <TabList>
+              <Tab>newest</Tab>
+              <Tab>activity</Tab>
+              <Tab>votes</Tab>
+            </TabList>
+            <TabPanel>newest</TabPanel>
+            <TabPanel>activity</TabPanel>
+            <TabPanel>votes</TabPanel>
+          </Tabs>
         </div>
       </div>
     )
