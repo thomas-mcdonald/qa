443c2eca6f02c3128d8184396f1faad43152cfc6
Thomas McDonald <thomas-mcdonald@users.noreply.github.com>
2016-12-17 13:34:16 +0000

    mock up the question list

--- ui/questions/index/Container.js
+++ ui/questions/index/Container.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import { Tab, Tabs, TabList, TabPanel } from '@blueprintjs/core'
 
+import QuestionList from './components/QuestionList'
+
 class Container extends Component {
   render() {
+    let questions = [{title: 'What a question'}, {title: 'How do I'}]
     return (
       <div id="page-wrap">
         <div id="body">
@@ -13,7 +16,7 @@ class Container extends Component {
               <Tab>activity</Tab>
               <Tab>votes</Tab>
             </TabList>
-            <TabPanel>newest</TabPanel>
+            <TabPanel><QuestionList questions={questions} /></TabPanel>
             <TabPanel>activity</TabPanel>
             <TabPanel>votes</TabPanel>
           </Tabs>
---
+++ ui/questions/index/components/QuestionList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'React'
+
+const QuestionCount = ({number, name}) =>
+  <div className="qa-question-list-count">{number} <span>{name}</span></div>
+
+const QuestionTag = ({children}) =>
+  <span className="qa-question-list-tag pt-tag pt-minimal">{children}</span>
+
+const Question = ({title}) => (
+  <div className="pt-card qa-question-list-card">
+    <div className="qa-question-list-counts">
+      <QuestionCount number="8" name="votes" />
+      <QuestionCount number="0" name="answers" />
+      <QuestionCount number="80" name="views" />
+    </div>
+    <div className="qa-question-list-summary">
+      <h3 className="qa-question-list-title">
+        <a>{title}</a>
+      </h3>
+      <div className="qa-question-list-tags">
+        <QuestionTag>meta</QuestionTag>
+        <QuestionTag>question</QuestionTag>
+        <QuestionTag>not-a-tag</QuestionTag>
+      </div>
+      <div className="qa-question-list-recent">
+        answered by <a>Thomas McDonald</a> three hours ago
+      </div>
+    </div>
+  </div>
+)
+
+class QuestionList extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.questions.map((question, i) => <Question key={i} {...question} />)}
+      </div>
+    )
+  }
+}
+
+export default QuestionList
